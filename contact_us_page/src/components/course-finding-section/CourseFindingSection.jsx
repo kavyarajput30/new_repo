7594cc66ee1b2947dd728f3b8b2af262483065c1
kavya rajput <131941640+rajputrajput30@users.noreply.github.com>
2023-10-03 19:07:39 +0530

    Add files via upload

---
+++ contact_us_page/src/components/course-finding-section/CourseFindingSection.jsx
@@ -0,0 +1,13 @@
+import './CourseFindingSection.css'
+
+const CourseFindingSection = () => {
+   return(
+    <div id="course-finding-main-div">
+     <h1 id="course-finding-heading">Let Career Corps help you find the Right Course and Right Job for you!</h1>
+     <div id="ellipse-courese-finding-heading"></div>
+   <button>I'm Intrested</button>
+    </div>
+   )
+}
+
+export default CourseFindingSection;
